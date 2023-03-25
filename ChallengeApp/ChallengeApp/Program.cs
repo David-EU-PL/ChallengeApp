fc8b2fae138b353f915f7fb5fd0040f8a0d5f85b
David-EU-PL <dawidplatek.eu.pl@outlook.com>
2023-03-25 22:07:24 +0100

    Zadanie domowe dzień 4.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+int age = 33;
+bool isMale = false;
+
+if (isMale == false && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (isMale == true && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("A o kogo chodzi ??? :)");
+}

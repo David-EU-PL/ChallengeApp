4ccee39f86e80c7fb4ec87ea7972527f8028edce
David-EU-PL <dawidplatek.eu.pl@outlook.com>
2023-05-04 00:16:52 +0200

    Test app 1. part

---
+++ ChallengeApp/ChallengeApp.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System.Reflection.Metadata;
+
+namespace ChallengeApp.Tests
+{
+    public class Tests
+    {
+       
+        [Test]
+        public void WhenTwoNumbersAreAdded_ShouldReturnSum()
+        {
+            // arrange
+            var user = new UserStringHandle()
+
+            // act
+           
+
+            // assert
+        
+        }
+    }
+}

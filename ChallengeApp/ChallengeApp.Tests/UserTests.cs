dc9a70c7100b05165cbfd5be7879310b08ae87b6
David-EU-PL <dawidplatek.eu.pl@outlook.com>
2023-05-06 22:59:52 +0200

    TypeTests

--- ChallengeApp/ChallengeApp.Tests/UserTests.cs
+++ ChallengeApp/ChallengeApp.Tests/UserTests.cs
@@ -1,25 +1,71 @@
-
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ChallengeApp.Tests
 {
     public class UserTests
     {
-       
         [Test]
-        public void WhenUserCollectTwoScores_ShouldCorrectResult()
+        public void FloatTypeTests()
         {
             // arrange
-            var user = new Employee("Adam", "Kowalski", "32");
-            user.AddScore(5);
-            user.AddScore(6);
-            user.AddScore(-2);
-            user.AddScore(-6);
+            float floatValue1 = 1.3f;
+            float floatValue2 = 1.3f;
 
             // act
-            var result = user.Result;
 
             // assert
-            Assert.AreEqual(3, result);
+            Assert.AreEqual(floatValue2, floatValue2);
+        }
+
+        [Test]
+        public void StringTypeTests()
+        {
+            // arrange
+            string user1 = "Dawid";
+            string user2 = "Dawid";
+
+            // act
+
+
+            // assert
+            Assert.AreEqual(user1, user2);
+        }
+
+        [Test]
+        public void IntTypeTests()
+        {
+            // arrange
+            int number1 = 10;
+            int number2 = 2;
+
+            // act
+
+            // assert
+            Assert.AreNotEqual(number1, number2);
+        }
+
+        [Test]
+        public void ReferenceTypeTests()
+        {
+            // arrange
+           var user1 = GetUser("Dawid");
+           var user2 = GetUser("Dawid");
+
+            // act
+            
+
+            // assert
+            Assert.AreNotEqual(user1, user2);
+        }
+
+        private Employee GetUser(string name)
+        {
+            return new Employee(name);
         }
     }
 }
--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -10,6 +10,12 @@
             this.Surname = surname;
             this.Age = age;
         }
+
+        public Employee(string name)
+        {
+            Name = name;
+        }
+
         public string Surname { get; private set; }
         public string Name { get; private set; }
         public string Age { get; private set; }
